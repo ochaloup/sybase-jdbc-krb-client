fb19f1be4c645a6711594686b5d01fb3c438d5fd
Martin Simka <msimka@redhat.com>
2014-09-24 15:25:04 +0200

    modified to use Subject.doAs

--- src/main/java/krb/test/sybase/Main.java
+++ src/main/java/krb/test/sybase/Main.java
@@ -1,14 +1,19 @@
 package krb.test.sybase;
 
-import com.sybase.jdbc4.jdbc.SybDriver;
+import com.sybase.jdbc4.jdbc.SybDataSource;
 
+import javax.security.auth.Subject;
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import javax.security.auth.login.LoginContext;
 import java.io.File;
+import java.security.PrivilegedExceptionAction;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Martin Simka
@@ -20,24 +25,62 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-            System.setProperty("java.security.auth.login.config", new File("login.conf").getAbsolutePath());
+        System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("java.security.krb5.realm", "MW.LAB.ENG.BOS.REDHAT.COM");
+        System.setProperty("java.security.krb5.kdc", "kerberos-test.mw.lab.eng.bos.redhat.com");
+        System.setProperty("java.security.krb5.conf", new File("krb5.conf").getAbsolutePath());
 
-            System.setProperty("java.security.krb5.realm", "MW.LAB.ENG.BOS.REDHAT.COM");
-            System.setProperty("java.security.krb5.kdc", "kerberos-test.mw.lab.eng.bos.redhat.com");
-            System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
-            //System.setProperty("java.security.krb5.conf", new File("krb5.conf").getAbsolutePath());
-            System.setProperty("sun.security.krb5.debug", "true");
+        class Krb5LoginConfiguration extends Configuration {
 
-            Properties props = new Properties();
-            //props.put("user", "KRBUSR01@MW.LAB.ENG.BOS.REDHAT.COM");
-            props.put("REQUEST_KERBEROS_SESSION", "true");
-            props.put("SERVICE_PRINCIPAL_NAME", "DB05@MW.LAB.ENG.BOS.REDHAT.COM");
+            private final AppConfigurationEntry[] configList = new AppConfigurationEntry[1];
 
-            DriverManager.registerDriver(new SybDriver());
-            Connection conn = DriverManager.getConnection(JDBC_URL, props);
-            printUserName(conn);
+            public Krb5LoginConfiguration() {
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("storeKey", "false");
+                options.put("useKeyTab", "true");
+                options.put("keyTab", "KRBUSR01");
+                options.put("principal", "KRBUSR01@MW.LAB.ENG.BOS.REDHAT.COM");
+                options.put("doNotPrompt", "true");
+                options.put("useTicketCache", "true");
+                options.put("ticketCache", "/tmp/krbcc_1000");
+                options.put("refreshKrb5Config", "true");
+                options.put("isInitiator", "true");
+                options.put("addGSSCredential", "true");
+                configList[0] = new AppConfigurationEntry(
+                        "org.jboss.security.negotiation.KerberosLoginModule",
+                        AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
+                        options);
+            }
 
-            conn.close();
+            @Override
+            public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+                if (name.equals("test")) {
+                    return configList;
+                }
+                return null;
+            }
+        }
+
+        Configuration.setConfiguration(new Krb5LoginConfiguration());
+        final LoginContext lc = new LoginContext("test");
+        lc.login();
+        Subject subject = lc.getSubject();
+
+        Connection conn = Subject.doAs(subject, new PrivilegedExceptionAction<Connection>() {
+            @Override
+            public Connection run() throws Exception {
+                SybDataSource ds = new SybDataSource();
+                ds.setREQUEST_KERBEROS_SESSION("true");
+                ds.setSERVICE_PRINCIPAL_NAME("DB05@MW.LAB.ENG.BOS.REDHAT.COM");
+                ds.setServerName("db05.mw.lab.eng.bos.redhat.com");
+                ds.setPortNumber(5000);
+                return ds.getConnection();
+            }
+        });
+
+        printUserName(conn);
+
+        conn.close();
     }
 
     private static void printUserName(Connection conn) throws SQLException {
